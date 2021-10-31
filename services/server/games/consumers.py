cd26b99a2204ad754df1e55568a3aed61229ce9a
parkyes90 <parkyes90@gmail.com>
2021-10-31 17:16:28 +0900

    feat: Add games consumer && routing

---
+++ services/server/games/consumers.py
@@ -0,0 +1,16 @@
+import json
+
+from channels.generic.websocket import WebsocketConsumer
+
+
+class GameConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, code):
+        pass
+
+    def receive(self, text_data=None, bytes_data=None):
+        data = json.load(text_data)
+        message = data["message"]
+        self.send(text_data=json.dumps({"message": message}))
