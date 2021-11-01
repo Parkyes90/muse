d000db0d0f21f20e00417ad9fd76870b42367932
parkyes90 <parkyes90@gmail.com>
2021-11-01 21:32:08 +0900

    feat: Add django async channel

--- services/server/games/consumers.py
+++ services/server/games/consumers.py
@@ -1,16 +1,35 @@
 import json
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
 class GameConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.group_name = None
+        self.room_name = None
+
     def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.group_name = f"games_{self.room_name}"
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name, self.channel_name
+        )
         self.accept()
 
     def disconnect(self, code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.group_name, self.channel_name
+        )
 
     def receive(self, text_data=None, bytes_data=None):
-        data = json.load(text_data)
+        data = json.loads(text_data)
         message = data["message"]
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name, {"type": "message", "message": message}
+        )
+
+    def message(self, event):
+        message = event["message"]
         self.send(text_data=json.dumps({"message": message}))
