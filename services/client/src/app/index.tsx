f6ea0485bd3126216ee9a69954c9cd7085fbe617
parkyes90 <parkyes90@gmail.com>
2021-11-01 21:32:21 +0900

    feat: Add client text form

--- services/client/src/app/index.tsx
+++ services/client/src/app/index.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { FormEvent, useEffect, useState } from "react";
+
+type Message = {
+  message: string;
+};
 
 function Index() {
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
+  useEffect(() => {
+    const socketInstance = new WebSocket(
+      `ws://${window.location.host}/ws/games/test/`
+    );
+    socketInstance.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log(data);
+      setMessages((prev) => [...prev, data]);
+    };
+    socketInstance.onclose = () => {
+      console.log("Socket Closed");
+    };
+    socketInstance.onopen = () => {
+      console.log("Socket Connected");
+    };
+
+    setSocket(socketInstance);
+  }, []);
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    socket?.send(
+      JSON.stringify({
+        message,
+      })
+    );
+  };
   return (
     <div>
-      app
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+      </form>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message.message}</div>
+        ))}
+      </div>
     </div>
   );
 }
