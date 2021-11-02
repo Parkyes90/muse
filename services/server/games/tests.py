ba54b0b16f9268c1bfd0aefdb58a86878ec8c26d
parkyes90 <parkyes90@gmail.com>
2021-11-02 20:29:00 +0900

    test: test send message

--- services/server/games/tests.py
+++ services/server/games/tests.py
@@ -11,3 +11,11 @@ class GameTests(ChannelsLiveServerTestCase):
         communicator = WebsocketCommunicator(application, "/ws/games/test")
         connected, _ = await communicator.connect()
         self.assertTrue(connected)
+
+    async def test_send_message(self):
+        communicator = WebsocketCommunicator(application, "/ws/games/test")
+        await communicator.connect()
+        message = "test"
+        await communicator.send_json_to({"message": message})
+        response = await communicator.receive_json_from()
+        self.assertEqual(response["message"], message)
