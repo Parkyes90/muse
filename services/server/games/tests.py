3648fca77523146672f4ce536c203b5a9cabc906
parkyes90 <parkyes90@gmail.com>
2021-11-02 20:37:55 +0900

    test: refactor common factor

--- services/server/games/tests.py
+++ services/server/games/tests.py
@@ -7,14 +7,18 @@ application = URLRouter(websocket_urlpatterns)
 
 
 class GameTests(ChannelsLiveServerTestCase):
-    async def test_connect(self):
+    async def get_connected_communicator(self):
         communicator = WebsocketCommunicator(application, "/ws/games/test")
-        connected, _ = await communicator.connect()
+        connect_result = await communicator.connect()
+        return communicator, connect_result
+
+    async def test_connect(self):
+        _, connect_result = await self.get_connected_communicator()
+        connected, _ = connect_result
         self.assertTrue(connected)
 
     async def test_send_message(self):
-        communicator = WebsocketCommunicator(application, "/ws/games/test")
-        await communicator.connect()
+        communicator, _ = await self.get_connected_communicator()
         message = "test"
         await communicator.send_json_to({"message": message})
         response = await communicator.receive_json_from()
