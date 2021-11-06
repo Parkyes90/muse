c031ff471708ecab25eac4c253264c099b2105b0
parkyes90 <parkyes90@gmail.com>
2021-11-06 18:13:34 +0900

    feat: Add Link & games-list

--- services/client/src/components/organisms/main-app-bar/index.tsx
+++ services/client/src/components/organisms/main-app-bar/index.tsx
@@ -1,20 +1,28 @@
 import React from "react";
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 import { AppBarProps } from "@mui/material/AppBar/AppBar";
+import { Link } from "react-router-dom";
 
 const MainAppBar: React.FC<AppBarProps> = (props) => {
   return (
     <AppBar position="static" {...props}>
       <Toolbar>
-        <LibraryMusicIcon />
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Muse
-        </Typography>
-        <Typography sx={{ flexGrow: 1 }}>
+        <Link to="/">
+          <Typography
+            variant="h6"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <LibraryMusicIcon />
+            Muse
+          </Typography>
+        </Link>
+        <Typography sx={{ marginLeft: "auto" }}>
           반주를 듣고 노래를 맞춰보세요!
         </Typography>
-        <Button color="inherit">Login</Button>
+        <Button color="inherit" sx={{ marginLeft: "auto" }}>
+          Login
+        </Button>
       </Toolbar>
     </AppBar>
   );
--- services/client/src/components/organisms/main-content/index.tsx
+++ services/client/src/components/organisms/main-content/index.tsx
@@ -1,56 +1,20 @@
-import {
-  Button,
-  Container,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  useTheme,
-} from "@mui/material";
-import { grey } from "@mui/material/colors";
+import { Box, BoxProps, Button } from "@mui/material";
 import React from "react";
-import { ContainerProps } from "@mui/material/Container/Container";
+import { Link } from "react-router-dom";
+import GamesList from "../games-list";
 
-const MainContent: React.FC<ContainerProps> = (props) => {
-  const theme = useTheme();
+const MainContent: React.FC<BoxProps> = (props) => {
   return (
-    <Container
-      sx={{ display: "flex", flexDirection: "column", flex: 1 }}
-      {...props}
-    >
-      <Button
-        type="button"
-        variant="outlined"
-        color="success"
-        sx={{ marginLeft: "auto" }}
-      >
-        게임 만들기
-      </Button>
-      <TableContainer
-        sx={{
-          marginTop: theme.typography.pxToRem(24),
-          padding: theme.typography.pxToRem(24),
-          border: `${theme.typography.pxToRem(1)} solid ${grey[300]}`,
-        }}
-      >
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>게임</TableCell>
-              <TableCell>참가 인원 수</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>추억의 투니버스 노래 맞추기</TableCell>
-              <TableCell>20</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Container>
+    <Box {...props} sx={{ display: "flex", flexDirection: "column" }}>
+      <Box sx={{ marginLeft: "auto" }}>
+        <Link to="games/create">
+          <Button type="button" variant="outlined" color="success">
+            게임 만들기
+          </Button>
+        </Link>
+      </Box>
+      <GamesList />
+    </Box>
   );
 };
 
