b9928c52bc1b278a96a7e7951430470746c94823
parkyes90 <parkyes90@gmail.com>
2021-11-11 22:20:11 +0900

    feat: Add drf parsers

--- services/server/config/settings.py
+++ services/server/config/settings.py
@@ -139,3 +139,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Rest framework
+REST_FRAMEWORK = {
+    "DEFAULT_PARSER_CLASSES": (
+        "rest_framework.parsers.FileUploadParser",
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+    ),
+}
