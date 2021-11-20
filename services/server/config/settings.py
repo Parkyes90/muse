43ad6c0f38b76221cb7852b7735503c09d989202
parkyes90 <parkyes90@gmail.com>
2021-11-20 15:43:43 +0900

    feat: Add drf pagination

--- services/server/config/settings.py
+++ services/server/config/settings.py
@@ -153,6 +153,8 @@ REST_FRAMEWORK = {
         "djangorestframework_camel_case.parser.CamelCaseJSONParser",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 SPECTACULAR_SETTINGS = {
