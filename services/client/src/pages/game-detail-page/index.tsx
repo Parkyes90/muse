b1274c1d0c4e14552d9f81ce2306335854b78312
parkyes90 <parkyes90@gmail.com>
2021-11-23 21:09:17 +0900

    feat: Add game detail items

--- services/client/src/pages/game-detail-page/index.tsx
+++ services/client/src/pages/game-detail-page/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import { AxiosError } from "axios";
+import { Box, Paper, TextField, Typography, useTheme } from "@mui/material";
 import { GameDetail } from "../../apis/types";
 import { requestFetchGame } from "../../apis/requests";
 
 const GameDetailPage: React.FC = () => {
   const params = useParams<"gameId">();
+  const theme = useTheme();
   const { data, isLoading, error } = useQuery<GameDetail, AxiosError>(
     `game-detail-${params.gameId}`,
     () => requestFetchGame(+params!.gameId!),
@@ -17,7 +19,43 @@ const GameDetailPage: React.FC = () => {
   if (isLoading || !data) {
     return <div>Loading...</div>;
   }
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant="h2">{data.title}</Typography>
+      <Typography variant="body1">{data.description}</Typography>
+      <Paper
+        elevation={1}
+        sx={{
+          mt: 3,
+          width: "100%",
+          height: theme.typography.pxToRem(200),
+        }}
+      >
+        힌트
+      </Paper>
+      <Paper
+        elevation={3}
+        sx={{
+          mt: 3,
+          width: "100%",
+          height: theme.typography.pxToRem(400),
+        }}
+      >
+        로그
+      </Paper>
+      <Box sx={{ mt: 3, width: "100%" }}>
+        <form>
+          <TextField label="노래 제목 입력하기" fullWidth />
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default GameDetailPage;
